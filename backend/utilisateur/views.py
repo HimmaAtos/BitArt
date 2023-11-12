57e548b63207336cea1a7c46e838f716a2d990f9
Mouhamed GUEYE <mouhamed.gueye.external@worldline.com>
2023-11-12 20:57:58 +0000

    login backend ready

--- backend/utilisateur/views.py
+++ backend/utilisateur/views.py
@@ -13,3 +13,32 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = Utilisateur.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Utilisateur introuvable!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
